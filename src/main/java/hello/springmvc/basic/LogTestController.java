c0f544db6848d0412088b44e8da33998b93a7e1d
byun <rpdla88203@gmail.com>
2023-10-27 23:48:15 +0900

    HTTP 요청 매핑

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,2 +1,25 @@
-package hello.springmvc.basic;public class LogTestController {
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Slf4j
+@RestController
+public class LogTestController {
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        System.out.println("name = " + name);
+        log.debug("info debug = {}", name);
+        log.info("info log = {}", name);
+        log.warn("info warn = {}", name);
+        log.error("info error = {}", name);
+
+        return "ok";
+    }
 }
